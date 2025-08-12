2a66d4c9aabe97896aae64ee0d95fd31ec6c73d3
Adarsh Jaiswal <adarsh.jaiswal@pixacore.com>
2025-08-12 22:12:12 +0530

    changes

--- src/components/Accreditations.jsx
+++ src/components/Accreditations.jsx
@@ -6,7 +6,7 @@ export default function Accreditations() {
       };
   return (
     <section id="accreditations" className="md:pt-[150px] pt-[120px] accreditations relative grid">
-        <img className="xl:block hidden absolute bottom-0 left-0" src={assetUrl('images/dhoniWithMan.png')} alt="" />
+        <img className="xl:block hidden absolute bottom-0 left-0 max-w-[52%]" src={assetUrl('images/dhoniWithMan.png')} alt="" />
         <img className="xl:hidden bottom-0 left-0 w-full" src={assetUrl('images/dhonipersonipad.png')} alt="" />
         <div className="container mx-auto px-4 h-full text-white grid xl:justify-end xl:text-left text-center items-center xl:pb-[135px] gap-[30px] xl:order-none order-first md:pb-0 pb-4">
             <h2 className="secTitle pb-2">Need Help Choosing <br /> a Scan Or Package? </h2>
@@ -18,7 +18,7 @@ export default function Accreditations() {
                     Enquire Now
                     <img className="md:block hidden" src={assetUrl('images/whiteArrow.svg')} alt="" />
                 </a>
-                <a href="javascript:;" className="themeBtn green">
+                <a href="javascript:;" className="themeBtn green invisible">
                     <img src={assetUrl('images/wa.svg')} alt="" />
                     <div className="wa">
                         <p>WhatsApp</p>
