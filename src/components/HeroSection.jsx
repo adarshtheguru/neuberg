b8d85919345940226ad7916adcf57bd3688d3751
Pragnesh <pragnesh@gmail.com>
2025-08-14 15:12:50 +0530

    banner line changed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,10 +20,10 @@ export default function HeroSection() {
     <section id="home" className="heroWrap pt-[7rem] flex-grow min-h-screen relative grid">
       <div className="container mx-auto px-4 grid xl:items-center items-start xl:text-left text-center">
         <div className="bannerCont xl:pt-0 md:pt-[50px]">
-          <div className="bannerPref capitalize pb-5 xl:font-semibold">
+          <div className="capitalize font-bold bannerCity fadeBlack">
             From Health checks to Advanced scans
           </div>
-          <div className="capitalize font-bold bannerCity fadeBlack">
+          <div className="bannerPref capitalize pb-5 xl:font-semibold">
             with Speed, Accuracy, <br className="visible-md" /><br className="hidden-md" /> and Trusted care.
           </div>
        {/*   <div className="bannerSvg hidden xl:block">
